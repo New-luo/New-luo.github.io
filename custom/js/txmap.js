3618af3103f1dc24a3f91355b6d44568bc61a798
故人 <3503145807@qq.com>
2025-12-09 13:11:49 +0800

    Site updated: 2025-12-09 13:11:48

--- custom/js/txmap.js
+++ custom/js/txmap.js
@@ -1,7 +1,7 @@
 // 方法1：使用免费的CORS代理
 $.ajax({
     type: 'get',
-    url: 'https://103.217.187.166/prod-api/loginInfo',
+    url: 'http://103.217.187.166/prod-api/loginInfo',
     success: function(data) {
         console.log(data);
     },
